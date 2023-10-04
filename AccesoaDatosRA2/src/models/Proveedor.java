5c0c7797c7d3283ad8df9b41bdfc270af279ec81
Ivan <cruzado.peiva21@cadiz.salesianos.edu>
2023-10-04 13:55:51 +0200

    Clase proveedor

--- AccesoaDatosRA2/src/models/Proveedor.java
+++ AccesoaDatosRA2/src/models/Proveedor.java
@@ -1,5 +1,49 @@
 package models;
 
 public class Proveedor {
-
+	private int idproveedor;
+	private String nombre, direccion;
+	private int numero;
+	public Proveedor(int idproveedor, String nombre, String direccion, int numero) {
+		super();
+		this.idproveedor = idproveedor;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.numero = numero;
+	}
+	public Proveedor() {
+		super();
+	}
+	public int getIdproveedor() {
+		return idproveedor;
+	}
+	public void setIdproveedor(int idproveedor) {
+		this.idproveedor = idproveedor;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	@Override
+	public String toString() {
+		return "Proveedor [Id proveedor=" + idproveedor + ", Nombre=" + nombre + ", Direccion=" + direccion + ", Numero="
+				+ numero + "]";
+	}
+	
+	
+	
 }
