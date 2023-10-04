bc4427774dd418a72223ad143d066d204e2dae03
José Manuel Reyes Jiménez <98025586+Josema35RJ@users.noreply.github.com>
2023-10-04 14:02:52 +0200

    Update Empleado

--- AccesoaDatosRA2/src/models/Empleado.java
+++ AccesoaDatosRA2/src/models/Empleado.java
@@ -1,5 +1,46 @@
 package models;
 
 public class Empleado {
+	private int iduser;
+	private String username, password;
 
+	public Empleado(int iduser, String username, String password) {
+		super();
+		this.iduser = iduser;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Empleado() {
+		super();
+	}
+
+	public int getIduser() {
+		return iduser;
+	}
+
+	public void setIduser(int iduser) {
+		this.iduser = iduser;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [iduser=" + iduser + ", username=" + username + ", password=" + password + "]";
+	}
 }
