7ffcbfe132639dadb14046da64e65eb29187379c
José Manuel Reyes Jiménez <98025586+Josema35RJ@users.noreply.github.com>
2023-10-04 14:12:40 +0200

    Create Transaccion

---
+++ AccesoaDatosRA2/src/models/Transaccion.java
@@ -0,0 +1,58 @@
+package models;
+
+import java.util.Date;
+
+public class Transaccion {
+	private int idinventario, idproducto, idproveedor;
+	private Date fecha;
+
+	public Transaccion(int idinventario, int idproducto, int idproveedor, Date fecha) {
+		super();
+		this.idinventario = idinventario;
+		this.idproducto = idproducto;
+		this.idproveedor = idproveedor;
+		this.fecha = fecha;
+	}
+
+	public Transaccion() {
+		super();
+	}
+
+	public int getIdinventario() {
+		return idinventario;
+	}
+
+	public void setIdinventario(int idinventario) {
+		this.idinventario = idinventario;
+	}
+
+	public int getIdproducto() {
+		return idproducto;
+	}
+
+	public void setIdproducto(int idproducto) {
+		this.idproducto = idproducto;
+	}
+
+	public int getIdproveedor() {
+		return idproveedor;
+	}
+
+	public void setIdproveedor(int idproveedor) {
+		this.idproveedor = idproveedor;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaccion [idinventario=" + idinventario + ", idproducto=" + idproducto + ", idproveedor="
+				+ idproveedor + ", fecha=" + fecha + "]";
+	}
+}
