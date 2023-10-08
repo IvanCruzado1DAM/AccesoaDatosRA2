23811859332dbaa116da09587c44abdfb7ad6dd8
Lorena <lorenacruzjurado@gmail.com>
2023-10-08 13:31:50 +0200

    mostrar Productos

--- AccesoaDatosRA2/src/views/CrudProducto.java
+++ AccesoaDatosRA2/src/views/CrudProducto.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,68 +16,88 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 
+import models.Producto;
+import services.ConexionBDSql;
+import services.ObjectService;
+
 public class CrudProducto extends JFrame {
-	
+
 	private JFrame ProductWindow;
 	private JLabel ProductLabel;
-	private JButton AddProduct,DeleteProduct,ModifyProduct,Exit;
-	
+	private JButton AddProduct, DeleteProduct, ModifyProduct, Exit;
+
 	private static JTable ProductTable;
 	private DefaultTableModel ProductCombo;
 	private JScrollPane ProductScroll;
 	private JPanel ProductPanel;
 
-	Manejador mane=new Manejador();
-	
-	public CrudProducto() {
+	ObjectService OS = new ObjectService();
+
+	Manejador mane = new Manejador();
+
+	public CrudProducto() throws ClassNotFoundException, SQLException {
 		createWindow();
 	}
 
-	private void createWindow() {
+	private void createWindow() throws ClassNotFoundException, SQLException {
 		// Ventana principal
 		ProductWindow = new JFrame("Administrar productos");
 		ProductWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ProductWindow.setBounds(100, 100, 650, 519);
 		ProductWindow.setLocationRelativeTo(null);
-		ProductWindow.getContentPane().setLayout(null);	
+		ProductWindow.getContentPane().setLayout(null);
 		// Texto menu admin
 		ProductLabel = new JLabel("Menu productos");
 		ProductLabel.setToolTipText("texto eleccion");
 		ProductLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		ProductLabel.setBounds(145, -10, 223, 66);
 		ProductWindow.add(ProductLabel);
-		//Botones
+		// Botones
 		createButtons();
-		//tabla
+		// tabla
 		createTable();
 		ProductWindow.setVisible(true);
 	}
 
-	private void createTable() {
+	private void createTable() throws SQLException {
 		ProductPanel = new JPanel();
 		ProductPanel.setLayout(null);
 		ProductPanel.setBounds(200, 0, 433, 463);
-		//Crear el JTable
-		//int id, String nombre, Float precio, String img, int proveedorid, int stock, String categoria
-		String[] columnas = new String[] { "ID", "Nombre", "Precio", "Img", "Id Proveedor", "Categoria" };
-		ProductCombo = new  DefaultTableModel(columnas, 0){
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return false; // Hace que todas las celdas no sean editables
-            }
-        };
+		// Crear el JTable
+		// int id, String nombre, Float precio, String img, int proveedorid, int stock,
+		// String categoria
+		String[] columnas = new String[] { "ID", "Nombre", "Marca", "Precio", "Img", "Id Proveedor", "Categoria" };
+		ProductCombo = new DefaultTableModel(columnas, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false; // Hace que todas las celdas no sean editables
+			}
+		};
 		setTablaProducto(new JTable(ProductCombo));
 		getTablaProducto().setPreferredScrollableViewportSize(new Dimension(250, 100));
 		getTablaProducto().getTableHeader().setReorderingAllowed(true);
 		getTablaProducto().setEnabled(true);
-		//imagenes
-		//		getTablaProfesores().getSelectionModel().addListSelectionListener(mi);
-		
+		// imagenes
+		// getTablaProfesores().getSelectionModel().addListSelectionListener(mi);
+
 		// Crear el ordenador de filas
 		TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(ProductCombo);
 		getTablaProducto().setRowSorter(sorter);
 		sorter.sort();
 		// rellenar Productos
+		List<Producto> listP;
+		try {
+			listP = OS.getAllProducts(ConexionBDSql.obtener());
+
+			for (Producto p : listP) {
+				Object[] data = { p.getIdproducto(), p.getNombre(), p.getMarca(), p.getPrecio(), p.getImg(),
+						p.getProveedorid(), p.getCategoria() };
+				ProductCombo.addRow(data);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //				for (Profesor pro : allProfesor) {
 //					Object[] data = { pro.getId(), pro.getImagen(), pro.getNombre(), pro.getDireccion(), pro.getAsignatura(),
 //							pro.getNumeroclases() };
@@ -91,34 +112,34 @@ public class CrudProducto extends JFrame {
 	}
 
 	private void createButtons() {
-		//añadir
+		// añadir
 		AddProduct = new JButton("ADD");
 		AddProduct.setBounds(525, 300, 100, 34);
 		AddProduct.addActionListener(mane);
 		ProductWindow.getContentPane().add(AddProduct);
-		//modificar
+		// modificar
 		ModifyProduct = new JButton("MODIFY");
 		ModifyProduct.setBounds(525, 340, 100, 34);
 		ModifyProduct.addActionListener(null);
 		ProductWindow.getContentPane().add(ModifyProduct);
-		//eliminar
+		// eliminar
 		DeleteProduct = new JButton("DELETE");
 		DeleteProduct.setBounds(525, 380, 100, 34);
 		DeleteProduct.addActionListener(null);
 		ProductWindow.getContentPane().add(DeleteProduct);
-		//volver
-		Exit = new JButton ("EXIT");
+		// volver
+		Exit = new JButton("EXIT");
 		Exit.setBounds(540, 438, 77, 34);
 		ProductWindow.getContentPane().add(Exit);
-		
-		
+
 	}
-	private class Manejador implements ActionListener{
+
+	private class Manejador implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			Object obj=e.getSource();
-			if(obj==AddProduct) {
+			Object obj = e.getSource();
+			if (obj == AddProduct) {
 				try {
 					new AddModifyProduct();
 				} catch (ClassNotFoundException | SQLException e1) {
@@ -126,20 +147,20 @@ public class CrudProducto extends JFrame {
 					e1.printStackTrace();
 				}
 				ProductWindow.setVisible(false);
-			}else if(obj==DeleteProduct) {
-				
-			}else if(obj==ModifyProduct) {
-				
-			}else if(obj==Exit) {
-				//volver al menu principal
-				//Principal();
-				//Cerrar ventana crud producto
+			} else if (obj == DeleteProduct) {
+
+			} else if (obj == ModifyProduct) {
+
+			} else if (obj == Exit) {
+				// volver al menu principal
+				// Principal();
+				// Cerrar ventana crud producto
 				ProductWindow.dispose();
 			}
 		}
-		
+
 	}
-	
+
 	public static JTable getTablaProducto() {
 		return ProductTable;
 	}
